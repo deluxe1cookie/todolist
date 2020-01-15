66ba8de584c5c8b6bf78391d0edabefa29b16b77
deluxe1cookie <32510967+deluxe1cookie@users.noreply.github.com>
2020-01-15 20:14:33 +0300

    Updates

---
+++ precache-manifest.20c4dba1ae21f8e70613fa9713967a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e101e571efc3ed83facd477da63d67",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "67c7d2c2f2dc34ebede5",
+    "url": "/todolist/static/css/main.5dd32053.chunk.css"
+  },
+  {
+    "revision": "9dce918a0ef1436c6f89",
+    "url": "/todolist/static/js/2.790409c0.chunk.js"
+  },
+  {
+    "revision": "67c7d2c2f2dc34ebede5",
+    "url": "/todolist/static/js/main.04cd0b8f.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
